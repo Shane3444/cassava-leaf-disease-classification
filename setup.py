4535d67ede3ad09798809267aa18df79eecb26ce
Shane3444 <shourya3444@gmail.com>
2023-09-10 15:48:13 +0530

    folder structure UPDATED

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as file:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "cassava-leaf-disease-classification"
